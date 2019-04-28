792a6b21b2ac2add9d22bfb4c7d8f1d1b265de49
Nicholas Mangano <nicholas.mangano@students.mq.edu.au>
2019-04-29 03:08:59 +1000

    Adding comments and Javadoc.

--- src/com/company/ClientSocket.java
+++ src/com/company/ClientSocket.java
@@ -3,7 +3,6 @@ package com.company;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
@@ -12,7 +11,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-// Class for implementing the client side communication for the project.
+/**
+ * <h1>Client Side Simulator for Distributed Systems</h1>
+ * <p>This class consists of methods that operate to implement a client side simulator for scheduling decisions for a
+ * distributed network. It implements a number of basic socket communication functions including:
+ * <ul>
+ *     <li>Establishing a connection to a job server that is responsible for feeding information to the simulator.</li>
+ *     <li>Sending messages over the socket.</li>
+ *     <li>Receiving Messages over the socket.</li>
+ * </ul>
+ * <p>In addition to this it also implements a number of scheduling algorithms:
+ * <ul>
+ *     <li>AllToLargest - As the name implies simply allocates every job to the largest server available. This is
+ *     considered the default mode of operation and if no other algorithm is specified will default to this.</li>
+ *     <li>FirstFit - This algorithm allocates jobs to the smallest server that is capable of running the job, and in
+ *     the event it cannot be allocated simply queues it to be executed on the smallest server that is capable of
+ *     running it.</li>
+ *     <li>BestFit - This Algorithm calculates a goodness of fit value specified by no. of cores required vs no
+ *     available on the server and leverages this against the time that a server is available, and in the event it
+ *     cannot be allocated simply queues it to be executed on the smallest server that is capable of running it. This
+ *     aims to make use of a large number of small servers to accommodate every job.</li>
+ *     <li>WorstFit - This Algorithm calculates a goodness of fit value specified by no. of cores required vs no
+ *     available on the server and leverages this against the time that a server is available, and in the event it
+ *     cannot be allocated simply queues it to be executed on the smallest server that is capable of running it. This
+ *     aims to make use of a small number of large servers to accommodate every job.</li>
+ * </ul>
+ *
+ * @author Nicholas Mangano
+ * @version 2.1
+ */
 public class ClientSocket {
 
     //Host name and communication port for socket communication.
@@ -33,7 +60,14 @@ public class ClientSocket {
     private BufferedWriter outC;
     private BufferedReader inC;
 
-    // Constructor that instantiates the input and output streams as well as connecting to the server.
+    /**
+     * Class constructor that takes several parameters and sets up the initial socket connection and pre-defines the
+     * algorithm to be used for the invocation of the class.
+     *
+     * @param hostName server host name.
+     * @param port port to make the socket connection over.
+     * @param algorithm algorithm to be used (atl, ff, bf or wf).
+     */
     public ClientSocket (String hostName, int port, String algorithm){
 
         // Sets the client as running and specifying the hostname and port to connect over.
@@ -52,8 +86,12 @@ public class ClientSocket {
         }
     }
 
-    // Function for reading incoming messages. Prints the message to the terminal and returns a string
-    // containing the message.
+    /**
+     * Socket function for reading incoming messages. Prints the received message to the terminal and returns a String
+     * containing the contents of the message.
+     *
+     * @return message received from the server.
+     */
     private String readMessage(){
         try {
             String message = inC.readLine();
@@ -65,7 +103,12 @@ public class ClientSocket {
         return "message error";
     }
 
-    // Function for sending messages. Prints the message to the terminal.
+    /**
+     * Socket function for sending messages to the server. Takes the message to be sent as an input and prints the
+     * message to the terminal once it has been sent.
+     *
+     * @param message String to send to the server.
+     */
     private void sendMessage(String message){
         try {
             outC.write(message + "\n");
@@ -76,7 +119,10 @@ public class ClientSocket {
         }
     }
 
-    // Closes the open connection and readers/writers.
+    /**
+     * Socket function for closing the open connection and terminating the readers and writers that are used to buffer
+     * incoming and outgoing messages.
+     */
     private void stopConnection(){
         try {
             inC.close();
@@ -87,7 +133,11 @@ public class ClientSocket {
         }
     }
 
-    // Main function for running the client and scheduling jobs.
+    /**
+     * Main function responsible for running the scheduler. Operates based on the protocol discussed in the
+     * documentation provided and implements many of the socket communication protocols and helper functions for
+     * running the algorithms.
+     */
     public void runClient (){
 
         // Initial authentication protocol
@@ -111,6 +161,11 @@ public class ClientSocket {
     }
 
     // Job scheduling function responsible for parsing messages related to jobs.
+
+    /**
+     * Main function for scheduling jobs, runs for every job request received from the server and implements one of
+     * four algorithms described above.
+     */
     private void jobSchedule(){
 
         // Takes the inital job request and splits on whitespace into an array of strings.
@@ -153,9 +208,19 @@ public class ClientSocket {
         }
     }
 
-    // Parses data sent from the server into an arraylist of string arrays for use in algorithm.
-    private ArrayList<String[]> createDataStruct() {
+    /**
+     * Function responsible for reacting data structures out of message requests for resources or resource availability
+     * as defined in the communication protocol. Takes as input a message of the form "RESC" or "LSTJ" with their proper
+     * fields and returns an array list of String arrays which contains either a resource list or a list of jobs running
+     * on a server.
+     *
+     * @param message controls what data the server sends.
+     * @return an array list of String arrays that contains requested data.
+     */
+    private ArrayList<String[]> createDataStruct(String message) {
         ArrayList<String[]> result = new ArrayList<String[]>();
+
+        this.sendMessage(message);
 
         if (this.readMessage().equals("DATA")) {
             this.sendMessage("OK");
@@ -176,7 +241,10 @@ public class ClientSocket {
         return result;
     }
 
-    // Reads in the system.xml using the SAX parser and stores the information in an arraylist of string arrays.
+    /**
+     * Function responsible for reading in the system.xml file. Makes use of a SAC parser and stores the data into the
+     * class level variable systemXML as an array list of string arrays tokenised on whitespace.
+     */
     private void readXML(){
         systemXML = new ArrayList<String[]>();
         try {
@@ -209,6 +277,13 @@ public class ClientSocket {
 
     // Algorithm to find the largest server by iterating over the system.xml file based on the server core count
     // (measurement of how large the server is) and returns the type of the largest server.
+
+    /**
+     * Algorithm to find the largest server available and allocate all jobs to it. Iterates over the system.xml data
+     * and determines size based on the servers core count. Returns the first largest servers type and index.
+     *
+     * @return largest server type and index.
+     */
     private String[] allToLargest(){
 
         int currentSize = 0;
@@ -223,40 +298,68 @@ public class ClientSocket {
         return new String[] {type, "0"};
     }
 
+    /**
+     * One of the main algorithm functions for the client side simulator. Returns a a server scheduling decision for a
+     * given job in the form of a String array of size two with the String at index 0 being the server type (e.g. "Small")
+     * and the String at index two being a non-negative integer (e.g. 0, 1, ... n).
+     * <p>
+     * This function will attempt to allocate the job to the smallest server that the job can be run on, iterating over
+     * decisions from smallest to largest. In the event the job cannot be run on any server schedules it to be preformed
+     * on the smallest server whose initial resources meet the requirements of the job.
+     *
+     * @param jobN a job to be scheduled in the format "JOBN submit_time job_ID est_runtime cores memory disk".
+     * @return schedulingDecision a string array that contains the server type and server index.
+     */
     private String[] firstFit(String[] jobN){
 
+        // Creates variables to keep track of which server to allocate to in the event all servers are active and
+        // cannot fit the job.
         String [] backupServer = new String[] {"", ""};
-        boolean initalRun = true;
+        boolean initialRun = true;
 
+        // Sorts systemXml in ascending order based on core count.
         Collections.sort(this.systemXML, new Comparator<String[]>() {
             public int compare(String[] string, String[] otherString) {
                 return Integer.parseInt(string[4]) - Integer.parseInt(otherString[4]);
             }
         });
 
+        // Iterates over all the server types from smallest to largest which is achievable as systemXML has been sorted
+        // in ascending order.
         for (int i = 0; i < systemXML.size(); i++){
+
+            // Checks if the job is capable of fitting on the server type assuming it has no other jobs running to pre
+            // check what server types the job can fit onto to reduce calls to the server for resource information.
             if ((Integer.parseInt(systemXML.get(i)[4]) >= Integer.parseInt(jobN[4])) &&
                     (Integer.parseInt(systemXML.get(i)[5]) >= Integer.parseInt(jobN[5]))&&
                     (Integer.parseInt(systemXML.get(i)[6]) >= Integer.parseInt(jobN[6]))){
 
-                if (initalRun) {
+                // Captures the smallest server that the job can be run on to be returned in the event all servers
+                // are currently active and cannot fit the job.
+                if (initialRun) {
                     backupServer = new String[]{systemXML.get(i)[0], "0"};
-                    initalRun = false;
+                    initialRun = false;
                 }
 
-                this.sendMessage("RESC Type " + systemXML.get(i)[0]);
-                this.resourceList = createDataStruct();
+                // Sends a message to the server for the resource information for the given server size and creates an
+                // array list of string arrays with each item in the array list being one server of type the queried
+                // type.
+                this.resourceList = createDataStruct("RESC Type " + systemXML.get(i)[0]);
 
+                // Iterates over the resource list to attempt to fit the job onto a server.
                 for (int k = 0; k < Integer.parseInt(systemXML.get(i)[1]); k++){
                     if ((Integer.parseInt(resourceList.get(k)[4]) >= Integer.parseInt(jobN[4])) &&
                             (Integer.parseInt(resourceList.get(k)[5]) >= Integer.parseInt(jobN[5]))&&
                             (Integer.parseInt(resourceList.get(k)[6]) >= Integer.parseInt(jobN[6]))){
 
+                        // Returns the server.
                         return new String[] {resourceList.get(k)[0], resourceList.get(k)[1]};
                     }
                 }
             }
         }
+        // If it cannot fit the job on a server allocates it to the first active server with the minimum initial
+        // resources to run the job.
         return backupServer;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+/**
+ * Main driving class for the ClientSocket class and handles command line parsing and creating of the scheduling
+ * instance based on the inputs.
+ *
+ * @author Nicholas Mangano
+ * @version 2.0
+ */
 public class Main {
     // Main class for the client side simulation, implements the ClientSocket class.
+
+    /**
+     * Function that parses the command line arguments and creates and instance of ClientSocket based on the user inputs
+     * provided at runtime.
+     *
+     * @param args takes the form [-a] algorithm
+     */
     public static void main(String[] args) {
         ClientSocket c;
 
+        // Switch statement for handling command line arguments.
         switch (args.length) {
+
+            // Case if option is provided with no input, or incorrect option provided.
             case 1:
                 if (args[0].equals("-a")){
                     System.out.println("Missing option -a");
@@ -15,6 +32,7 @@ public class Main {
                     System.exit(0);
                 }
 
+            // Case if option is provided with input correct and incorrect.
             case 2:
                 if (args[0].equals("-a") && (args[1].equals("ff") || args[1].equals("bf") ||args[1].equals("wf"))){
                     c = new ClientSocket("127.0.0.1", 8096, args[1]);
